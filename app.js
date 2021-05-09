1604e7aa3b1fbda69dd346f22d9e597220154c4a
Prathamesh-1610 <prathameshprataprane@gmail.com>
2021-05-09 16:40:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//jshint esversion:6
+const express= require("express");
+const bodyParser = require('body-parser');
+const http=require("http");
+const request = require('request');
+var bagName;
+var query;
+let json;
+
+const app= express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+  res.render("index",{proDetails:bagName, barCode:query});
+  query="";bagName="";
+
+});
+
+app.post("/",function(req,res){
+ query=req.body.WarrantyNo;
+const url="http://210.210.25.108/VipServices/api/AccessApi/CheckDetail?Barcode="+query;
+
+http.get(url,function(response){
+
+response.on("data",function(data){
+//
+const responseData=JSON.parse(data);
+bagName=responseData.Message[0].Msg;
+console.log(responseData);
+//res.write("Product description:");
+res.redirect("/");
+});
+});
+
+});
+
+app.listen(4000, function(){
+  console.log("Server is ruuning");
+});
